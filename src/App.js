e7f60eae74ba5df01167550d4a617f91c9a175a5
agentallandev <allanep9@gmail.com>
2022-09-27 11:25:19 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Sky } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
+
+function App() {
+  return (
+    <>
+      <div>Outside Canvas</div>
+      <Canvas>
+        <Sky sunPosition={[100, 100, 20]}/>
+      </Canvas>
+    </>
+  );
+}
+
+export default App;
