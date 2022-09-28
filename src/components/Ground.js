977a8cd2183119c40e3331495300226e5afe3b51
agentallan <allanep9@gmail.com>
2022-09-28 12:37:03 -0400

    code update

--- src/components/Ground.js
+++ src/components/Ground.js
@@ -1,6 +1,6 @@
 import { usePlane } from "@react-three/cannon"
 import { NearestFilter, RepeatWrapping } from "three"
-import  {groundTexture } from '../images/textures'
+import  { groundTexture } from '../images/textures'
 export const Ground = () => {
     const [ref] = usePlane (() => ({
         rotation: [-Math.PI/2, 0, 0], position: [0, 0, 0]
